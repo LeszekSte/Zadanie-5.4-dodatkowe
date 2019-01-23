52c759472af49512787061ac72ec2ca0b62391a7
LeszekS <leszekstepien@o2.pl>
2019-01-23 12:52:24 +0100

    Versja 1

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,7 @@
 public class Main {
+    public static void main(String[] args) {
+        System.out.print((char)75);
+        System.out.print((char)79);
+        System.out.print((char)84);
+    }
 }
